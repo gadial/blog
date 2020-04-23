122aeda9df462aa354a657c1ad8eca28430d2510
Gadi Aleksandrowicz <gadia@il.ibm.com>
2020-04-23 13:16:07 +0300

    Ignored files

--- bot/twitter_bot.py
+++ bot/twitter_bot.py
@@ -3,14 +3,19 @@ import yaml
 import random
 import re
 import json
+import logging
 
 class Bot:
     MAX_TWEET_SIZE = 280
 
-    def __init__(self, data_filename):
-        self.data_filename = data_filename
+    def __init__(self, name):
+        self.data_filename = name + ".json"
+        self.logging_filename = name + ".log"
+        logging.basicConfig(filename=self.logging_filename, level=logging.INFO, format='%(asctime)s %(message)s')
+        logging.info("Bot started")
         self.load_data()
         self.connect()
+        logging.info("Init complete")
     
     def connect(self):
         auth_data = self.data['auth']
@@ -26,12 +31,31 @@ class Bot:
         with open(self.data_filename, 'w', encoding='utf8') as file:
             json.dump(self.data, file)
 
+    def update_data(self):
+        for s in self.api.mentions_timeline(count=100):
+            status_data = {
+                'text': s.text,
+                'reply_to': s.in_reply_to_status_id,
+                'user_id': s.user.id,
+                'user_name': s.user.name,
+                'user_screen_name': s.user.screen_name
+            }
+            self.data['mentions'][s.id] = status_data
+        self.save_data()
+
     def tweet(self, text):
         print("About to tweet '" + text + "'")
         if len(text) > self.MAX_TWEET_SIZE:
             print("Tweet too long: ", len(text))
             return
         self.api.update_status(text)
+    
+    def tweet_new_post(self, post_file):
+        with open("../_posts/" + post_file, encoding='utf8') as file:
+            desc = re.search('twitter_description: "(.*)"', file.read()).group(1)
+        text = "פוסט חדש בבלוג:" + '\n' + desc + '\n' + self.name_to_link(post_file)
+        self.tweet(text)
+
 
     def reply(self, text, id):
         self.api.update_status(status = text, in_reply_to_status_id = id , auto_populate_reply_metadata=True)
@@ -46,7 +70,7 @@ class Bot:
         url = SITE + post['url']
         description = post['text']
         text = "והיום בפינת פוסט המבוא:" + '\n' + description + '.' + '\n' + url
-        bot.tweet(text)
+        self.tweet(text)
 
     def post_long_text(self, long_text):
         MAX_LENGTH = 270
@@ -73,7 +97,7 @@ class Bot:
     def reply_to_questions(self, start_id):
         with open('questions.yml', encoding='utf8') as file:
             questions = yaml.load(file, Loader=yaml.FullLoader)
-        tweets_to_check = list(reversed(bot.api.mentions_timeline(start_id, count=100)))
+        tweets_to_check = list(reversed(self.api.mentions_timeline(start_id, count=100)))
         for t in tweets_to_check:
             print(t.text[::-1])
         for q in questions:
@@ -83,5 +107,24 @@ class Bot:
                     self.reply(response, t.id)
                     print("Replied to ", t.id)
 
-bot = Bot('not_precise.json')
+    def name_to_link(self, filename):
+        (year, month, day, name) = re.search(r'(\d\d\d\d)-(\d\d)-(\d\d)-(.*)\.md', filename).groups()
+        return self.data['site'] + "/" + year + "/" + month + "/" + day + "/" + name + "/"
+        
+
+
+
+bot = Bot('not_precise')
+bot.tweet_new_post('2020-01-24-what_is_mipstar_equals_re_part_2.md')
+
+# bot = Bot('gadial')
+# bot.tweet_post('http://gadial.net/2020/01/20/what_are_percents/', '2020-01-20-what_are_percents.md')
+
+# tweets = tweepy.Cursor(bot.api.user_timeline, tweet_mode='extended').items(20)
+# for tweet in tweets:
+#     content = tweet.full_text
+#     print(content)
+
+# bot.update_data()
+# print(bot.data)
 # bot.reply_to_questions(1217488783907676161)
