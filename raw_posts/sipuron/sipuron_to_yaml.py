875537fa45a019f155465f9a009f2cc24b79c880
gadial <gadial@gmail.com>
2021-11-02 16:45:16 +0200

    Sipuron search

--- raw_posts/sipuron/sipuron_to_yaml.py
+++ raw_posts/sipuron/sipuron_to_yaml.py
@@ -1,19 +1,30 @@
 from typing_extensions import runtime
 import yaml,csv
 
-result = []
+stories = []
+keywords = []
+authors = []
 with open("sipuron.csv", encoding="utf8") as csvfile:
     datareader = csv.reader(csvfile, delimiter=",", quotechar='"')
     for row_index, row in enumerate(datareader):
         if row_index == 0:
             continue 
-        result.append({
+        stories.append({
             'time': row[0], 
             'author': row[1], 
             'keyword': row[2], 
             'story': row[3],
             'length': len(row[3])
             })
+        if row[1] not in authors:
+            authors.append(row[1])
+        if row[2] not in keywords:
+            authors.append(row[2])
 
+result = {
+    'result': stories,
+    'keywords': keywords,
+    'authors': authors
+}
 with open("sipuron.yml", 'w', encoding="utf8") as yamlfile:
     yaml.dump(result, yamlfile, allow_unicode=True)
