db28fea6875210aae51459ae252b7057eabad074
Gadi Aleksandrowicz <gadia@il.ibm.com>
2022-11-04 14:57:18 +0200

    Sipuron 2022 first update

--- raw_posts/sipuron/sipuron_to_yaml.py
+++ raw_posts/sipuron/sipuron_to_yaml.py
@@ -8,23 +8,29 @@ with open("sipuron.csv", encoding="utf8") as csvfile:
     datareader = csv.reader(csvfile, delimiter=",", quotechar='"')
     for row_index, row in enumerate(datareader):
         if row_index == 0:
-            continue 
+            continue
+        if row[2] == '':
+            keyword = ''
+        else:
+            keyword = row[2].split(" ")[1]
         stories.append({
             'time': row[0], 
             'author': row[1], 
-            'keyword': row[2], 
+            'keyword': keyword, 
             'story': row[3],
             'length': len(row[3])
             })
         if not row[1] in authors:
             authors.append(row[1])
-        if not row[2] in keywords:
-            keywords.append(row[2])
+        if not keyword in keywords:
+            keywords.append(keyword)
 
 result = {
-    'stories': stories,
-    'keywords': keywords,
-    'authors': authors
+    'year_2022': {
+        'stories': stories,
+        'keywords': keywords,
+        'authors': authors
+    }
 }
-with open("sipuron.yml", 'w', encoding="utf8") as yamlfile:
+with open("sipuron2022.yml", 'w', encoding="utf8") as yamlfile:
     yaml.dump(result, yamlfile, allow_unicode=True)
