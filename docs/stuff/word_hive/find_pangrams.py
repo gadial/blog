7aa3b89065b5a6f79e653f745869702db3065cdf
gadial <gadial@gmail.com>
2023-07-22 21:22:09 +0300

    Fix bug in panagrams file

--- docs/stuff/word_hive/find_pangrams.py
+++ docs/stuff/word_hive/find_pangrams.py
@@ -1,6 +1,16 @@
 import json
+def lower(word):
+    lower_map = {
+        'ך': 'כ',
+        'ם': 'מ',
+        'ן': 'נ',
+        'ף': 'פ',
+        'ץ': 'צ',
+    }
+    return "".join([lower_map.get(c, c) for c in word])
+
 with open("dictionary.json", "r", encoding="utf8") as f:
     words = json.load(f)
-pangrams = [word for word in words if len(set(word)) == 7]
+pangrams = [lower(word) for word in words if len(set(lower(word))) == 7]
 with open("pangrams.json", "w", encoding="utf8") as f:
     json.dump(pangrams, f, ensure_ascii=False)
